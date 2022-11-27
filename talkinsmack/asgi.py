231a781c3475608578b830eb06f88e36feb5eea8
mohamed khan <mohamedkhan@mohameds-MacBook-Pro.local>
2022-11-27 04:38:12 -0500

    first commit

---
+++ talkinsmack/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for talkinsmack project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'talkinsmack.settings')
+
+application = get_asgi_application()
