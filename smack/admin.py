d661c0d6735cd94ee817428736b043aefe36b3c2
mohamed khan <mohamedkhan@mohameds-MacBook-Pro.local>
2022-11-27 18:04:12 -0500

    added comment model

--- smack/admin.py
+++ smack/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from .models import Smack
+from .models import Smack, Comment
 
 
 @admin.register(Smack)
 class SmackAdmin(admin.ModelAdmin):
     list_display = ['title', 'author', 'text', 'created_at', 'updated_at']
+    list_filter = ['created_at', 'updated_at']
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['smack', 'author', 'body', 'created_at', 'updated_at']
     list_filter = ['created_at', 'updated_at']
--- smack/models.py
+++ smack/models.py
@@ -7,11 +7,25 @@ User = get_user_model()
 
 
 class Smack(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE,blank=True, null=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, blank=True, null=True)
     title = models.CharField(max_length=30)
     text = models.CharField(max_length=300)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
+    
     def __str__(self):
         return f"Smack : {self.title} By:  {self.author}"
+
+
+class Comment(models.Model):
+    smack = models.ForeignKey(
+        Smack, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name='comments', default=None, blank=True, null=True)
+    body = models.TextField(max_length=300)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'Comment {self.body} by {self.author}'
